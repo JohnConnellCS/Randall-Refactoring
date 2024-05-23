dccdc746dc96917a933976c89dff7f2a7ff84bd6
CONNELL <johnc@lnxsrv15.seas.ucla.edu>
2024-05-22 17:06:23 -0700

    refactored most of randall.c into output.c and modified include statements accordingly

--- output.c
+++ output.c
@@ -17,5 +17,50 @@ writebytes (unsigned long long x, int nbytes)
 
 int handle_output(int nbytes)
 {
-    return nbytes;
+    /* Now that we know we have work to do, arrange to use the
+     appropriate library.  */
+  void (*initialize) (void);
+  unsigned long long (*rand64) (void);
+  void (*finalize) (void);
+  if (rdrand_supported ())
+    {
+      initialize = hardware_rand64_init;
+      rand64 = hardware_rand64;
+      finalize = hardware_rand64_fini;
+    }
+  else
+    {
+      initialize = software_rand64_init;
+      rand64 = software_rand64;
+      finalize = software_rand64_fini;
+    }
+
+  initialize ();
+  int wordsize = sizeof rand64 ();
+  int output_errno = 0;
+
+  do
+    {
+      unsigned long long x = rand64 ();
+      int outbytes = nbytes < wordsize ? nbytes : wordsize;
+      if (!writebytes (x, outbytes))
+	{
+	  output_errno = errno;
+	  break;
+	}
+      nbytes -= outbytes;
+    }
+  while (0 < nbytes);
+
+  if (fclose (stdout) != 0)
+    output_errno = errno;
+
+  if (output_errno)
+    {
+      errno = output_errno;
+      perror ("output");
+    }
+
+  finalize ();
+  return !!output_errno;
 }
